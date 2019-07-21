0b0e4f31b545e6805296ac5ee8e6e25fdee6f5b6
ciciou <2207679962@qq.com>
2019-07-21 23:14:13 +0800

    modify reverse.py

--- reverse.py
+++ reverse.py
@@ -1,2 +1,12 @@
-L=[1,2,7,10]
-print(L)
+class Solution:
+    def reverse(self,x:int)->int:
+        y=abs(x)
+        result=0
+        while y>0:
+            result=result*10+y%10
+            y//=10
+        result=result if result < (1<<31)-1 else 0
+        return -result if x<0 else result
+if __name__=='__main__':
+    x=120
+    print(Solution().reverse(x))
