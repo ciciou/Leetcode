4f0e97ead1b672c03b9853af2c797016ca88d96a
lizhenyu <985818479@qq.com>
2019-08-04 11:20:25 +0800

    add solution

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution():
     def longestCommonPrefix(self,strs:List[str])->str:
-        #思路：利用zip
+        #思路：利用zip,zip函数:将对象中对应的元素打包成一个个元组
         result=''
         for tup in zip(*strs):
             count=0
@@ -15,6 +15,7 @@ class Solution():
         return result
 
     def longestCommonPrefix_1(self,strs:List[str])->str:
+        #set函数：返回一个无无序不重复元素集
         result=''
         for tup in zip(*strs):
             if len(set(tup))!=1:
@@ -22,6 +23,18 @@ class Solution():
             result=f"{result}{tup[0]}"
         return result
 
+    def longestCommonPrefix_2(self,strs:List[str])->str:
+        if not strs:
+            return ""
+        max_str=max(strs)
+        min_str=min(strs)
+        print(max_str)
+        print(min_str)
+        for index,char in enumerate(min_str):
+            if char!=max_str[index]:
+                return min_str[:index]
+        return min_str
+
 if __name__=='__main__':
     str1=['cici','ciabby','cigray']
-    print(Solution().longestCommonPrefix_1(strs=str1))
+    print(Solution().longestCommonPrefix_2(strs=str1))
