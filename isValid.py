8d19fe7449ffedd15128fa4dfa27c88cfa56278a
lizhenyu <985818479@qq.com>
2019-08-04 17:51:09 +0800

    有效括号

---
+++ isValid.py
@@ -0,0 +1,16 @@
+class Solution():
+    def isValid(self,s:str)->bool:
+        # 思路：数组模拟栈，遍历字符串与栈顶元素对比，配对上进行出栈操作，否则入栈操作
+        if not s:
+            return True
+        map = {'(': ')', '{': '}', '[': ']'}
+        result = []
+        for index in range(len(s)):
+            if len(result)==0 or s[index]!=map.get(result[len(result)-1],""):
+                result.append(s[index])
+            else:
+                result.pop()
+        return not bool(result)
+
+if __name__=='__main__':
+    print(Solution().isValid("()"))
