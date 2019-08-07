834980d39f28e7d59a23ada1c68b1eca4d877d28
ciciou <2207679962@qq.com>
2019-08-07 14:27:36 +0800

    add solution

--- strStr.py
+++ strStr.py
@@ -2,7 +2,7 @@
 #author:cici
 class Solution():
     def strStr(self,haystack:str,needle:str)->int:
-        #思路：find()检查字符串中是否包含子字符串str
+        #思路：调用库函数find()检查字符串中是否包含子字符串str
         if needle==[]:
             return -1
         else:
@@ -10,10 +10,19 @@ class Solution():
                 return haystack.find(needle)
             else:
                 return -1
+
+    def strStr_1(self,haystack:str,needle:str)->int:
+        #思路：暴力法，时间复杂度：O((m-n)n)
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)]==needle:
+                return i
+            else:
+                pass
+        return -1
 if __name__=='__main__':
     haystack='hello'
     needle='ll'
-    print(Solution().strStr(haystack=haystack,needle=needle))
+    print(Solution().strStr_1(haystack=haystack,needle=needle))
 
 
 
