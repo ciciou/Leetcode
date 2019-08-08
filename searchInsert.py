a3bb12c64301fc176b05de5b55d1cefb48f53a8d
ciciou <2207679962@qq.com>
2019-08-08 15:45:15 +0800

    searchInsert.py

--- searchInsert.py
+++ searchInsert.py
@@ -1,2 +1,14 @@
 #-*-coding: utf-8 -*-
 #author:cici
+from typing import List
+class Solution():
+    def searchInsert(self,nums:List[int],target:int)->int:
+        for index,num in enumerate(nums):
+            if target<=num:
+                return index
+            else:
+                pass
+
+if __name__=='__main__':
+    print(Solution().searchInsert([1,3,6,7],5))
+
